64f91183877239664a896c90f5dc3b356ef625bf
Tal Hadad <TalHadad@users.noreply.github.com>
2021-12-09 17:38:10 +0200

    squash! Raspberry pi pin controller

--- raspberry_pi_controller.py
+++ raspberry_pi_controller.py
@@ -1,16 +1,22 @@
 # raspberry_pi_controller.py
 
 import RPi.GPIO as GPIO
+import time
 # define pins numbering scheme (physical (board) or gpio (bcm))
 GPIO.setmode(GPIO.BOARD)
 # (or) GPIO.setmode(GPIO.BCM)
 
-# define output pins
-GPIO.setup(11, GPIO.OUT)
+def main():
+    # define output pins
+    GPIO.setup(11, GPIO.OUT)
 
-# send signal throght output pin
-GPIO.output(11, True) # or 1 instead of True
-#GPIO.output(11, False) # or 0 instead of False
+    # send signal throght output pin
+    GPIO.output(11, True) # or 1 instead of True
+    #GPIO.output(11, False) # or 0 instead of False
 
-# release definition from pins
-GPIO.cleanup()
+    time.sleep(20)
+    # release definition from pins
+    GPIO.cleanup()
+
+if __name__ == '__main__':
+    main()
