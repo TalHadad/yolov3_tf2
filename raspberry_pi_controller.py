e79a008f84b461a93818490cc47d2593998c1a36
Tal Hadad <TalHadad@users.noreply.github.com>
2021-12-28 17:23:49 +0200

    Update raspi controller to PWMs

--- raspberry_pi_controller.py
+++ raspberry_pi_controller.py
@@ -2,21 +2,35 @@
 
 import RPi.GPIO as GPIO
 import time
-# define pins numbering scheme (physical (board) or gpio (bcm))
+# define pins/pwms numbering scheme (physical (board) or gpio (bcm))
 GPIO.setmode(GPIO.BOARD)
 # (or) GPIO.setmode(GPIO.BCM)
 
 def main():
     print('starting GPIO 11 and 13.')
-    # define output pins
-    GPIO.setup(11, GPIO.OUT)
-    GPIO.setup(13, GPIO.OUT)
+    # define output pwms
+    pwm_nums = np.array([11,12,13,15])
+    for num in pwm_nums:
+        GPIO.setup(num, GPIO.OUT)
 
     # send signal throght output pin
-    GPIO.output(11, True) # or 1 instead of True
-    GPIO.output(13, True) # or 1 instead of True
+    #GPIO.output(11, True) # or 1 instead of True
     #GPIO.output(11, False) # or 0 instead of False
 
+    # controll the signal voltage (dim)
+    pwms = set()
+    for num in pwm_nums:
+        # PWM(pin, freq) pin_num/GPIO_num, requency
+        pwm = GPIO.PWM(num, 1000)
+        # start(dutyCycle), dutyCycle = [0,100] continuous range
+        pwm.start(100)
+        #  ChangeFrequency(freq) freq = new frequency in Hertz
+        #pwm.ChangeFrequency(freq)
+        # ChangeDutyCycle(dutyCycle) dutyCycle = [0,100] continuous range
+        #pwm.ChangeDutyCycle(100)
+        pwms.add(pwm)
+
+    # send UP signal for 60 seconds
     i=0
     seconds = 60
     while i<seconds:
@@ -24,7 +38,10 @@ def main():
         print(f'sleep second {i}/{seconds}')
         time.sleep(1)
 
-    # release definition from pins
+
+    # release definition from pwms
+    for pwm in pwms:
+        pwm.stop()
     GPIO.cleanup()
 
 if __name__ == '__main__':
