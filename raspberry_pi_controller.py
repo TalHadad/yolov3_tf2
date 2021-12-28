47c8a776cafb7d541aeec2a3fa7a2ad04203aa3d
Tal Hadad <talhadad189@gmail.com>
2021-12-28 14:22:29 +0200

    Add prints to raspi controller

--- raspberry_pi_controller.py
+++ raspberry_pi_controller.py
@@ -7,14 +7,23 @@ GPIO.setmode(GPIO.BOARD)
 # (or) GPIO.setmode(GPIO.BCM)
 
 def main():
+    print('starting GPIO 11 and 13.')
     # define output pins
     GPIO.setup(11, GPIO.OUT)
+    GPIO.setup(13, GPIO.OUT)
 
     # send signal throght output pin
     GPIO.output(11, True) # or 1 instead of True
+    GPIO.output(13, True) # or 1 instead of True
     #GPIO.output(11, False) # or 0 instead of False
 
-    time.sleep(20)
+    i=0
+    seconds = 60
+    while i<seconds:
+        i+=1
+        print(f'sleep second {i}/{seconds}')
+        time.sleep(1)
+
     # release definition from pins
     GPIO.cleanup()
 
